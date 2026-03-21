5e966a8b53330f7f18f3b0fe83ebd88ed7ecf355
redhunt07 <redhunt07@tutanota.com>
2026-03-21 22:11:37 +0100

    clean

--- _tmp_stats.sh
+++
@@ -1,3 +0,0 @@
-cd /home/debian/Fedi3
-TOK=$(sed -n 's/^FEDI3_RELAY_ADMIN_TOKEN=//p' .env | head -n1)
-curl -sS -H "Authorization: Bearer $TOK" https://relay.fedi3.com/_fedi3/relay/stats | python3 -m json.tool
